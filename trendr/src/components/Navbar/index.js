0aee40d9da629111dbbb93bdfa793c96135981c6
Cooper <eliteswimmer42@gmail.com>
2020-12-14 21:21:46 -0600

    starting to add various components based on the projects issues tab on GitHub

---
+++ trendr/src/components/Navbar/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <a className="navbar-brand" href="/">
+        Trendr
+      </a>
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ trendr/src/pages/trend_generation.js
@@ -0,0 +1,78 @@
+// Utilize trend_generation.js for the buttonclick function on the 6 different categorical buttons
+
+// I.E. --- For Example
+
+
+// import React, { useEffect, useState } from "react";
+// import API from "../utils/API";
+// import CardContainer from "../components/CardContainer";
+// import Row from "../components/Row";
+
+// function Gallery() {
+//   const [user, setUser] = useState({});
+//   const [users, setUsers] = useState([]);
+//   const [userIndex, setUserIndex] = useState(0);
+
+//   // When the component mounts, a call will be made to get random users.
+//   useEffect(() => {
+//     loadUsers();
+//   }, []);
+
+//   function nextUser(userIndex) {
+//     // Ensure that the user index stays within our range of users
+//     if (userIndex >= users.length) {
+//       userIndex = 0;
+//     }
+//     setUser(users[userIndex]);
+//     setUserIndex(userIndex);
+//   }
+
+//   function previousUser(userIndex) {
+//     // Ensure that the user index stays within our range of users
+//     if (userIndex < 0) {
+//       userIndex = users.length - 1;
+//     }
+//     setUser(users[userIndex]);
+//     setUserIndex(userIndex);
+//   }
+
+//   function handleBtnClick(event) {
+//     // Get the title of the clicked button
+//     const btnName = event.target.getAttribute("data-value");
+//     if (btnName === "next") {
+//       const newUserIndex = userIndex + 1;
+//       nextUser(newUserIndex);
+//     } else {
+//       const newUserIndex = userIndex - 1;
+//       previousUser(newUserIndex);
+//     }
+//   }
+
+//   function loadUsers() {
+//     API.getLanguagesList()
+//       .then(languages => {
+//         API.getUsersByLanguage(languages[0]).then(users => {
+//           setUsers(users);
+//           setUser(users[0]);
+//         });
+//       })
+//       .catch(err => console.log(err));
+//   }
+
+//   return (
+//     <div>
+//       <h1 className="text-center">Welcome to LinkedUp</h1>
+//       <h3 className="text-center">Click on the arrows to browse users</h3>
+//       <Row>
+//         <CardContainer
+//           title={user.login}
+//           image={user.image}
+//           language={user.language}
+//           handleBtnClick={handleBtnClick}
+//         />
+//       </Row>
+//     </div>
+//   );
+// }
+
+// export default Gallery;
