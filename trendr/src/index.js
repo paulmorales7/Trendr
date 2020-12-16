811fe0b9c57e6b2705e2107205f517e3e9863726
Cooper <eliteswimmer42@gmail.com>
2020-12-15 20:33:46 -0600

    installing material-iu bootstrap and attempting to get the landing page set up

--- trendr/src/index.js
+++ trendr/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import Dashboard from "./Dashboard";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Dashboard />, document.getElementById("root"));
