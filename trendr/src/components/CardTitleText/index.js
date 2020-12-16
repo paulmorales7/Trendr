9836a1f62ea9d435d21faebd4962755814c4481a
Cooper <eliteswimmer42@gmail.com>
2020-12-15 19:05:03 -0600

    header and footer. the other components will have to wait. starting with what works first

--- trendr/src/components/CardTitleText/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function CardTitleText({ title }) {
-  return <h2>{title}</h2>;
-}
-
-export default CardTitleText;
--- trendr/src/components/Navbar/index.js
+++ trendr/src/components/Navbar/index.js
@@ -4,7 +4,7 @@ import "./style.css";
 function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <a className="navbar-brand" href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&list=RDdQw4w9WgXcQ&start_radio=1&t=1" target="_blank">
+      <a className="navbar-brand" href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&list=RDdQw4w9WgXcQ&start_radio=1&t=1" target="_blank" rel="noreferrer">
         Trendr
       </a>
     </nav>
