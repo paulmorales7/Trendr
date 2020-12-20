e8e2fc6e7b88fa567e8f78d39df0b82ca18e07b2
Bradley Guidry <bradleyguidry@gmail.com>
2020-12-20 12:36:16 -0600

    sets up trending tweets

--- backend/utils/gTrend.js
+++ backend/utils/gTrend.js
@@ -1,11 +1,12 @@
 const googleTrends = require('google-trends-api');
+const client = require('./twitter')
 
 function gTrend() { 
 
 googleTrends.dailyTrends({
     // trendDate: Date.now(),
-    geo: 'US'
-    // category: 'all'
+    geo: 'US',
+    category: 'all'
   }, function(err, results) {
     if (err) {
       console.log(err);
@@ -16,12 +17,15 @@ googleTrends.dailyTrends({
       // var resArticles = jsonRes.storySummaries.trendingStories[0].articles
       // var resTitle = jsonRes.storySummaries.trendingStories[0].
 
-      console.log(jsonRes.default.trendingSearchesDays[0].trendingSearches[0].title.query); // .storySummaries.trendingStories[0].title
-
+      console.log(jsonRes.default.trendingSearchesDays[0].trendingSearches[0].title.query); 
+      const trendTopic = jsonRes.default.trendingSearchesDays[0].trendingSearches[0].title.query
+      client.get('search/tweets', {q: trendTopic}, function(error, tweets, response) {
+        console.log(tweets);
+     });
       // return trendRes
     }
   });
 
 } 
 
-// module.exports = { gTrend };
+module.exports = { gTrend };
---
+++ backend/utils/twitter.js
@@ -0,0 +1,11 @@
+var Twitter = require('twitter');
+console.log(process.env)
+var client = new Twitter({
+  consumer_key: process.env.TWITTER_API_KEY,
+  consumer_secret: process.env.TWITTER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN,
+  access_token_secret: process.env.TWITTER_TOKEN_SECRET,
+
+  
+});
+module.exports = client;
