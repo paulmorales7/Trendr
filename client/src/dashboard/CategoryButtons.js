0f5778350f7b4280c2a42199d2690a45b8254f02
94Cooper94 <LCAhearn94@gmail.com>
2021-01-04 20:53:35 -0600

    centered headers!!

--- client/src/dashboard/CategoryButtons.js
+++ client/src/dashboard/CategoryButtons.js
@@ -10,61 +10,55 @@ function preventDefault(event) {
   event.preventDefault();
 }
 
+
 const useStyles = makeStyles({
   hroot: {
-    marginRight: '3%',
+    marginRight: '6%',
     fontWeight: 'bold',
     background: 'linear-gradient(45deg, #790909 30%, #ff4b22 90%)',
     boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-    color: 'white',
-    fontSize: '0.6rem'
+    color: 'white'
   },
   sroot: {
-    marginRight: '3%',
+    marginRight: '6%',
     fontWeight: 'bold',
     background: 'linear-gradient(45deg, #ff4b22 30%, #d6ce18 90%)',
     boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-    color: 'white',
-    fontSize: '0.6rem'
+    color: 'white'
   },
   mroot: {
-    marginRight: '3%',
+    marginRight: '6%',
     fontWeight: 'bold',
     background: 'linear-gradient(45deg, #d6ce18 30%, #32c613 90%)',
     boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-    color: 'white',
-    fontSize: '0.6rem'
+    color: 'white'
   },
   troot: {
-    marginRight: '3%',
+    marginRight: '6%',
     fontWeight: 'bold',
     background: 'linear-gradient(45deg, #32c613 30%, #5d45ff 90%)',
     boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-    color: 'white',
-    fontSize: '0.6rem'
+    color: 'white'
   },
   broot: {
-    marginRight: '3%',
+    marginRight: '6%',
     fontWeight: 'bold',
     background: 'linear-gradient(45deg, #5d45ff 30%, #00cfff 90%)',
     boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-    color: 'white',
-    fontSize: '0.6rem'
+    color: 'white'
   },
   eroot: {
-    marginRight: '3%',
+    marginRight: '6%',
     fontWeight: 'bold',
     background: 'linear-gradient(45deg, #00cfff 30%, #b545ff 90%)',
     boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-    color: 'white',
-    fontSize: '0.6rem'
+    color: 'white'
   },
   allroot: {
     fontWeight: 'bold',
     background: 'linear-gradient(45deg, #b545ff 40%, #790909 90%)',
     boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-    color: 'white',
-    fontSize: '0.6rem'
+    color: 'white'
   },
 });
 
@@ -74,16 +68,16 @@ export default function CategoryButtons(props) {
   return (
     <React.Fragment>
       <Title>Categories</Title>
-      <div id="flexcenter" style={{ marginLeft: '15%', marginRight: '15%' }}>
-        <Typography color="primary" className={classes.categoryIcon}>
+      <div style={{ marginLeft: '5%', marginRight: '5%' }}>
+        <Typography variant="button" color="primary" className={classes.categoryIcon}>
           <Toolbar variant="spaced" color="primary" aria-label="contained primary button group" onClick={preventDefault}>
-            <Button id="h" size='large' onClick={props.getTopData} className={classes.hroot}>Top</Button>
-            <Button id="s" size='large' onClick={props.getData} className={classes.sroot}>Sports</Button>
-            <Button id="m" size='large' className={classes.mroot}>Health</Button>
-            <Button id="t" size='large' className={classes.troot}>Technology</Button>
-            <Button id="b" size='large' onClick={props.getBusinessData} className={classes.broot}>Business</Button>
-            <Button id="e" size='large' className={classes.eroot}>Entertainment</Button>
-            <Button id="all" size='large' className={classes.allroot}>All</Button>
+            <Button id="h" size='large' onClick={props.getTopData} className={classes.hroot}><span className={classes.buttonText}>Top</span></Button>
+            <Button id="s" size='large' onClick={props.getData} className={classes.sroot}><span className={classes.buttonText}>Sports</span></Button>
+            <Button id="m" size='large' className={classes.mroot}><span className={classes.buttonText}>Health</span></Button>
+            <Button id="t" size='large' className={classes.troot}><span className={classes.buttonText}>Tech</span></Button>
+            <Button id="b" size='large' onClick={props.getBusinessData} className={classes.broot}><span className={classes.buttonText}>Biz</span></Button>
+            <Button id="e" size='large' className={classes.eroot}><span className={classes.buttonText}>Entmt</span></Button>
+            <Button id="all" size='large' className={classes.allroot}><span className={classes.buttonText}>All</span></Button>
           </Toolbar>
         </Typography>
       </div>
--- client/src/dashboard/Title.js
+++ client/src/dashboard/Title.js
@@ -1,10 +1,18 @@
 import * as React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import PropTypes from 'prop-types';
 import Typography from '@material-ui/core/Typography';
 
+const useStyles = makeStyles({
+  centeredText: {
+    textAlign: 'center',
+  }
+});
+
 function Title(props) {
+  const classes = useStyles();
   return (
-    <Typography component="h2" variant="h6" color="primary" gutterBottom>
+    <Typography className={classes.centeredText} component="h2" variant="h6" color="primary" gutterBottom>
       {props.children}
     </Typography>
   );
