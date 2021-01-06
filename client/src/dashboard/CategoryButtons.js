4c8efeadb49f0df8795755cb062d4a56a8e7f7cb
Bradley Guidry <bradleyguidry@gmail.com>
2021-01-05 22:32:27 -0600

    changes foont color to black

--- client/src/dashboard/CategoryButtons.js
+++ client/src/dashboard/CategoryButtons.js
@@ -22,46 +22,46 @@ const useStyles = makeStyles((theme) => ({
     shadowRadius: 60,
   },
   hroot: {
-    fontWeight: 'bold',
+    fontWeight: '50 px',
     background: 'linear-gradient(45deg, #790909 65%, #ff4b22 90%)',
     boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-    color: '#333333'
+    color: 'black'
   },
   sroot: {
     fontWeight: 'bold',
     background: 'linear-gradient(45deg, #ff4b22 65%, #d6ce18 90%)',
     boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-    color: '#333333'
+    color: 'black'
   },
   mroot: {
     fontWeight: 'bold',
     background: 'linear-gradient(45deg, #d6ce18 65%, #32c613 90%)',
     boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-    color: '#333333'
+    color: 'black'
   },
   troot: {
     fontWeight: 'bold',
     background: 'linear-gradient(45deg, #32c613 65%, #00cfff 90%)',
     boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-    color: '#333333'
+    color: 'black'
   },
   broot: {
     fontWeight: 'bold',
     background: 'linear-gradient(45deg, #00cfff 65%, #5d45ff 90%)',
     boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-    color: '#333333'
+    color: 'black'
   },
   eroot: {
     fontWeight: 'bold',
     background: 'linear-gradient(45deg, #5d45ff 65%, #b545ff 90%)',
     boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-    color: '#333333'
+    color: 'black'
   },
   allroot: {
     fontWeight: 'bold',
     background: 'linear-gradient(45deg, #b545ff 65%, #790909 90%)',
     boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-    color: '#333333'
+    color: 'black'
   },
   toolbar: {
     justifyContent: 'space-around',
