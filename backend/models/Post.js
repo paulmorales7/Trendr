e2a13881579e611bd79704f9f14db5d09301fe6a
Desmond Aldridge <bob@hyainc.com>
2021-01-11 21:44:18 -0600

    Recovered code from my previous merge from yesterday

--- backend/models/Post.js
+++ backend/models/Post.js
@@ -1,14 +1,7 @@
 const mongoose = require('mongoose');
 
 const postSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true
-  },
-  body: {
-    type: String,
-    required: true
-  },
+  body: String,
 });
 
 module.exports = mongoose.model('Post', postSchema)
