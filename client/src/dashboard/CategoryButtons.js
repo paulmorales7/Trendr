39562bef7558833c187054234c98c040b50e4b23
94Cooper94 <LCAhearn94@gmail.com>
2021-01-11 21:17:11 -0600

    git pull is rule

--- client/src/dashboard/CategoryButtons.js
+++ client/src/dashboard/CategoryButtons.js
@@ -73,9 +73,6 @@ const useStyles = makeStyles((theme) => ({
   }
 }));
 
-const setSportsData = () => {
-  localStorage.setItem('sports button', 'Sports')
-}
 
 export default function CategoryButtons(props) {
   const classes = useStyles();
@@ -85,13 +82,13 @@ export default function CategoryButtons(props) {
         <img src={trendrLogo} alt="trendr-logo" className={classes.logo} />
       </Toolbar>
       <Toolbar className={classes.toolbar} color="primary" aria-label="contained primary button group" onClick={preventDefault}>
-        <Button id="h" size='large' onClick={() => { props.getTopData(); props.saveHistory('Top') }} className={classes.hroot}><span className={classes.buttonText}>Top</span></Button>
-        <Button id="s" size='large' onClick={() => { props.getSportsData(); props.saveHistory('Sports') }} className={classes.sroot}><span className={classes.buttonText}>Sports</span></Button>
-        <Button id="m" size='large' onClick={() => { props.getHealthData(); props.saveHistory('Health') }} className={classes.mroot}><span className={classes.buttonText}>Health</span></Button>
-        <Button id="t" size='large' onClick={() => { props.getTechData(); props.saveHistory('Tech') }} className={classes.troot}><span className={classes.buttonText}>Tech</span></Button>
-        <Button id="b" size='large' onClick={() => { props.getBusinessData(); props.saveHistory('Biz') }} className={classes.broot}><span className={classes.buttonText}>Biz</span></Button>
-        <Button id="e" size='large' onClick={() => { props.getEntertainmentData(); props.saveHistory('Entmt') }} className={classes.eroot}><span className={classes.buttonText}>Entmt</span></Button>
-        <Button id="all" size='large' onClick={() => { props.getAllData(); props.saveHistory('All') }} className={classes.allroot}><span className={classes.buttonText}>All</span></Button>
+        <Button id="h" size='large' onClick={props.getTopData} className={classes.hroot}><span className={classes.buttonText}>Top</span></Button>
+        <Button id="s" size='large' onClick={props.getSportsData} className={classes.sroot}><span className={classes.buttonText}>Sports</span></Button>
+        <Button id="m" size='large' onClick={props.getHealthData} className={classes.mroot}><span className={classes.buttonText}>Health</span></Button>
+        <Button id="t" size='large' onClick={props.getTechData} className={classes.troot}><span className={classes.buttonText}>Tech</span></Button>
+        <Button id="b" size='large' onClick={props.getBusinessData} className={classes.broot}><span className={classes.buttonText}>Biz</span></Button>
+        <Button id="e" size='large' onClick={props.getEntertainmentData} className={classes.eroot}><span className={classes.buttonText}>Entmt</span></Button>
+        <Button id="all" size='large' onClick={props.getAllData} className={classes.allroot}><span className={classes.buttonText}>All</span></Button>
       </Toolbar>
     </React.Fragment>
   );
